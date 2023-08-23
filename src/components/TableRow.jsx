949236daf347d1834d3dec64622b1b22dab252e8
Caleb Ratelle <caleb.ratelle@gmail.com>
2023-08-23 16:54:59 -0600

    end of 8/23/23

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import ModeButtons from './ModeButtons.jsx';
+import Description from './Description.jsx';
+import Hours from './Hours.jsx';
+import Rate from './Rate.jsx';
+import formatCurrency from '../utils/formatCurrency.js';
 
-export default function InvoiceTableRow() {
+export default function TableRow({initialInvoiceData, initialIsEditing}) {
+  const {description, rate, hours} = initialInvoiceData
+
   return (
-    <div>InvoiceTableRow</div>
+    <tr>
+    <ModeButtons isEditing={initialIsEditing} />
+    <Description isEditing={initialIsEditing} value={description}/>
+    <Rate isEditing={initialIsEditing} value ={rate}/>
+    <Hours isEditing={initialIsEditing} value={hours}/>
+    <td>{formatCurrency(rate * hours)}</td>
+  </tr>
   )
 }
